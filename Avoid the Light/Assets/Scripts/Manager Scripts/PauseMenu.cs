0059bc14e32ad93df76753151ded65b4ea868461
gavinbaltar <gavin.baltar@ufl.edu>
2025-02-13 19:43:46 -0500

    Added Pause Menu and Tried Fixing Sprites
    UI Sprites are still kind of bugging, namely the border. Pause Menu added in Gameplay scene.

---
+++ Avoid the Light/Assets/Scripts/Manager Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused; // Can use the public static bool to prevent inputs by checking if isPaused is true or false in other scripts.
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
